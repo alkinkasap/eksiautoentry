3963b1187846bf5703683fe70743e71b73bba2d8
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-27 16:52:53 +0300

    remove sys.exit

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ fi
 cp "config.JSON" "$HOME/.eksiautoentry"
 cp "pp.py" "$HOME/.eksiautoentry"
 
-cat <(crontab -l) <(echo "0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> generated.log")  | crontab -
+cat <(crontab -l) <(echo "0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> $HOME/.eksiaoutentry/generated.log")  | crontab -
 
 echo "Installed under folder --> $HOME/.eksiautoentry"
 echo "DONE !"
