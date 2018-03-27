f12f36bbcf4d9cfea827226c810e0fd6e043f455
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-27 16:55:19 +0300

    fix path

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ fi
 cp "config.JSON" "$HOME/.eksiautoentry"
 cp "pp.py" "$HOME/.eksiautoentry"
 
-cat <(crontab -l) <(echo "0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> $HOME/.eksiaoutentry/generated.log")  | crontab -
+cat <(crontab -l) <(echo "0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> $HOME/.eksiautoentry/generated.log")  | crontab -
 
 echo "Installed under folder --> $HOME/.eksiautoentry"
 echo "DONE !"
