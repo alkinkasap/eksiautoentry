e4feab6a10611372f9b40042f0736728ffd2bb11
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-27 16:52:46 +0300

    fix path

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@ if [ -d "$HOME/.eksiautoentry" ]; then
   rm -rf "$HOME/.eksiautoentry"
 fi
 
-search_string="0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> generated.log"
+search_string="0 * * * * python $HOME/.eksiautoentry/pp.py $HOME/.eksiautoentry/config.JSON >> $HOME/.eksiautoentry/generated.log"
 crontab -l > tempcron
 ctr=1
 while IFS='' read -r line || [[ -n "$line" ]]; do
